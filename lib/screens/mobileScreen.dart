17469c989a129e01b38dd384d9815cf64e42b6d3
haseeb zafar <Haseebzafar107@gmail.com>
2021-04-12 00:18:17 +0500

    final product

--- lib/screens/mobileScreen.dart
+++ lib/screens/mobileScreen.dart
@@ -34,6 +34,7 @@ class _MobileScreenState extends State<MobileScreen> {
                       controller: controller,
                       decoration: InputDecoration(
                         labelText: "Your copied text",
+                        
                       ),
                     )
                 ),
